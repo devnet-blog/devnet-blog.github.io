e85b98811c8124f54d0ee07e9cbfbfb2428c6dfa
Kamil Urbanek <urbanek.kamil@gmail.com>
2022-01-20 11:53:54 +0100

    dev

--- tasks.py
+++ tasks.py
@@ -123,18 +123,6 @@ def livereload(c):
 
     server.serve(host=CONFIG['host'], port=CONFIG['port'], root=CONFIG['deploy_path'])
 
-
-@task
-def publish(c):
-    """Publish to production via rsync"""
-    pelican_run('-s {settings_publish}'.format(**CONFIG))
-    c.run(
-        'rsync --delete --exclude ".DS_Store" -pthrvz -c '
-        '-e "ssh -p {ssh_port}" '
-        '{} {ssh_user}@{ssh_host}:{ssh_path}'.format(
-            CONFIG['deploy_path'].rstrip('/') + '/',
-            **CONFIG))
-
 @task
 def gh_pages(c):
     """Publish to GitHub Pages"""
